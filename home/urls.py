3a16565154c292fc10edc81ccd1c7985e989ac79
hyunkyeng <hyunk4593@gmail.com>
2019-02-14 02:31:38 +0000

    09_Separate URL for mutiple apps

---
+++ home/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views  # 자기의 view를 불러오는 것\
+
+
+urlpatterns = [
+    path('static_example/', views.static_example, name='static_example'),
+    path('template_example', views.template_example, name='template_example'),
+    path('user_create/', views.user_create, name='user_create'),
+    path('user_new/', views.user_new, name='user_new'),
+    path('pong/', views.pong, name='pong'),
+    path('ping/', views.ping, name='ping'),
+    path('cube/<int:number>/', views.cube, name='cube'),
+    path('hello/<name>/', views.hello, name='hello'),
+    path('dinner/', views.dinner, name='dinner'),
+    path('index/', views.index, name='index'),
+    
+]
